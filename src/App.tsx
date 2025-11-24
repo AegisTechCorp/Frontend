83e766eabda2944d106740a6e170da249d851a05
Ace <ace92dev@gmail.com>
2025-11-24 14:02:17 +0100

    feat: ajouter la page d'inscription et la route correspondante

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './components/landing'
 import LoginPage from './pages/Login'
+import RegisterPage from './pages/Register'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
   )
