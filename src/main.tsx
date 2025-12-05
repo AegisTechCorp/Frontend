3eb140743e6a0f75d69b4d5a8f57590a42ab02e6
Dylan Zakaharinivo <zakadylan2005@gmail.com>
2025-12-05 11:23:14 +0100

    update console log to verify deployment version

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-console.log('Frontend version: v42')
+console.log('Frontend version: v43')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
